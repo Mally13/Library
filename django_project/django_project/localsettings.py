0a837fd9effa3cb5a6f37cdc370ff32c31bfc5c3
Mally13 <mallymutuku13@gmail.com>
2022-07-24 23:38:30 +0300

    new

---
+++ django_project/django_project/localsettings.py
@@ -0,0 +1,4 @@
+SECRET_KEY = 'django-insecure-!gj)&q7)6821g80+d=ybl06(4ytq#ezd+z=7u82ivf92vwa%46'
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
